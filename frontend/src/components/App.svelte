1ab13f1e33427a1a5b75387c15b8da4111b62c9a
Martin Solheim <martin.solheim@me.com>
2020-01-14 11:04:53 +0100

    Frontend for henting av inntekt
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>

--- frontend/src/components/App.svelte
+++ frontend/src/components/App.svelte
@@ -2,10 +2,12 @@
     import SlettPerson from './SlettPerson.svelte';
     import OpprettVedtaksperiode from './OpprettVedtaksperiode.svelte';
     import SectionButton from './SectionButton.svelte';
+    import HentInntekt from "./HentInntekt.svelte";
 
     const Section = {
         SLETT_PERSON: 'slettperson',
-        OPPRETT_VEDTAKSPERIODE: 'opprettvedtaksperiode'
+        OPPRETT_VEDTAKSPERIODE: 'opprettvedtaksperiode',
+        HENT_INNTEKT: "hentinntekt"
     };
 
     let activeSection = Section.SLETT_PERSON;
@@ -23,23 +25,32 @@
         isActive={activeSection === Section.OPPRETT_VEDTAKSPERIODE}
         onClick={() => activeSection = Section.OPPRETT_VEDTAKSPERIODE}
     />
+    <SectionButton
+        label="Hent inntekt"
+        isActive={activeSection === Section.HENT_INNTEKT}
+        onClick={() => activeSection = Section.HENT_INNTEKT}
+    />
 </header>
 <hr>
 {#if activeSection === Section.SLETT_PERSON}
     <SlettPerson />
 {:else if activeSection === Section.OPPRETT_VEDTAKSPERIODE}
     <OpprettVedtaksperiode />
+{:else if activeSection === Section.HENT_INNTEKT}
+    <HentInntekt />
 {/if}
 
 <style>
     header {
         padding: 0 1.5rem;
     }
+
     h1 {
         margin: 1.5rem 0;
         font-size: 2rem;
         font-weight: normal;
     }
+
     hr {
         border: none;
         border-top: 5px solid #dad6d2;
---
+++ frontend/src/components/HentInntekt.svelte
@@ -0,0 +1,52 @@
+<script>
+    import Form from "./Form.svelte";
+    import TextInput from "./TextInput.svelte";
+
+    let inntektField;
+    let aktørId = '';
+    let inntekt = {};
+
+    const onSubmit = async () => {
+        if (aktørId) {
+            const result = await fetch(`/person/inntekt/${aktørId}`, {
+                method: 'get',
+                headers: {"Content-Type": "application/json", "Accept": "application/json"}
+            });
+            inntekt = await result.json();
+        }
+    };
+
+    const copy = () => {
+        let clipboardTextarea = document.createElement("textarea");
+        document.body.appendChild(clipboardTextarea);
+        clipboardTextarea.value = inntekt.beregnetMånedsinntekt;
+        clipboardTextarea.select();
+        document.execCommand("copy");
+        clipboardTextarea.remove();
+    };
+</script>
+<Form onSubmit={onSubmit} submitText="Hent inntekt">
+    <TextInput bind:value={aktørId} label="Aktør-id" placeholder="Arbeidstakers aktør-id" />
+</Form>
+<p class="response">
+    {#if inntekt.beregnetMånedsinntekt}
+        Beregnet månedsinntekt:
+        <span class="response__result" on:click={copy}>{inntekt.beregnetMånedsinntekt} <i
+            class="material-icons-outlined">file_copy</i></span>
+    {/if}
+</p>
+
+<style>
+    .response {
+        padding-top: 1.5rem;
+        padding-left: 1.5rem;
+    }
+
+    .response__result:hover {
+        cursor: pointer;
+    }
+
+    .material-icons-outlined {
+        font-size: 1rem;
+    }
+</style>

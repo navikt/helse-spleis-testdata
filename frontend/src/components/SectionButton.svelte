55e61d01d7e2e4715a6fcc051632306a02b53325
Stephen Ramthun <stephenramthun@gmail.com>
2020-01-23 09:20:01 +0100

    Legg til flere animasjoner

--- frontend/src/components/SectionButton.svelte
+++ frontend/src/components/SectionButton.svelte
@@ -18,24 +18,26 @@
         font-size: 1rem;
         cursor: pointer;
         margin: 0;
+        transition: all 0.2s;
     }
-    button.active:after,
-    button:focus:after,
-    button:hover:after {
-        background: var(--active-color);
+    button:after {
         position: absolute;
         content: '';
         height: 5px;
         width: 100%;
         left: 0;
         bottom: -5px;
+        transition: all 0.2s;
     }
+    button.active:after,
+    button:focus:after,
+    button:hover:after {
+        background: var(--active-color);
+    }
+    button.active,
     button:hover,
     button:focus {
         outline: none;
         color: var(--active-color);
-    }
-    button.active {
-        font-weight: bold;
     }
 </style>

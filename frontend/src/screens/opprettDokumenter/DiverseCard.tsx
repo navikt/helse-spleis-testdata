ef1937e567de658a942928b946e0028204b1e588
Simen Ullern <simenullern@gmail.com>
2023-12-15 13:18:42 +0100

    Fjerner (Ingen) som mulig medlemskapsverdi
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Morten Tholander <morten.tholander@nav.no>

--- frontend/src/screens/opprettDokumenter/DiverseCard.tsx
+++ frontend/src/screens/opprettDokumenter/DiverseCard.tsx
@@ -16,7 +16,6 @@ export const DiverseCard = React.memo(() => {
           <FormSelect
               label="Medlemskapsvurdering"
               options={[
-                  { value: "", label: "(Ingen)" },
                   "JA",
                   "NEI",
                   "UAVKLART",

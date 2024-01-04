7b44fc1cae05b249a2f3dfbb41d4fb8d42ef3a96
david steinsland <david@davidsteinsland.net>
2024-01-04 10:14:44 +0100

    prøver å håndtere feil

--- frontend/src/screens/opprettDokumenter/PersonCard.tsx
+++ frontend/src/screens/opprettDokumenter/PersonCard.tsx
@@ -55,7 +55,7 @@ export const PersonCard = () => {
         .then((result) => result.json() )
         .then((response) => {
           if ('arbeidsforhold' !in response) return console.log(`ukjent response: `, response)
-          else setArbeidsgivere(() => response.arbeidsforhold.map((it) => {
+          else setArbeidsgivere(() => typeof response.arbeidsforhold !== 'undefined' ? response.arbeidsforhold.map((it) => {
             return {
               type: it.type,
               arbeidsgiver: {
@@ -68,7 +68,7 @@ export const PersonCard = () => {
                 }
               })
             } as Arbeidsgiver
-          }))
+          }) : [])
         })
   }, [fnr]);
 

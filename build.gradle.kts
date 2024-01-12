0ff2f899f215eb52c3f28a3d64c57a9903a6d898
david steinsland <david@davidsteinsland.net>
2024-01-12 09:48:03 +0100

    google artifact registry

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,26 @@ plugins {
     kotlin("jvm") version "1.9.22"
 }
 
-val junitJupiterVersion = "5.8.2"
-val ktorVersion = "2.0.1"
-val hikariVersion = "3.3.1"
-val flywayVersion = "6.1.3"
-val kotliqueryVersion = "1.3.0"
-val testContainersVersion = "1.16.3"
+val junitJupiterVersion = "5.10.1"
+val ktorVersion = "2.3.7"
 
 group = "no.nav.helse"
 
 repositories {
+    val githubPassword: String? by project
     mavenCentral()
+    /* ihht. https://github.com/navikt/utvikling/blob/main/docs/teknisk/Konsumere%20biblioteker%20fra%20Github%20Package%20Registry.md
+        så plasseres github-maven-repo (med autentisering) før nav-mirror slik at github actions kan anvende førstnevnte.
+        Det er fordi nav-mirroret kjører i Google Cloud og da ville man ellers fått unødvendige utgifter til datatrafikk mellom Google Cloud og GitHub
+     */
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/maven-release")
+        credentials {
+            username = "x-access-token"
+            password = githubPassword
+        }
+    }
     maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
-    maven("https://jitpack.io")
 }
 
 apply(plugin = "org.jetbrains.kotlin.jvm")
@@ -22,7 +29,7 @@ apply(plugin = "org.jetbrains.kotlin.jvm")
 val tbdLibsVersion: String by project
 dependencies {
     implementation("com.github.navikt.tbd-libs:azure-token-client-default:2024.01.09-20.20-d52bae29")
-    implementation("com.github.navikt:rapids-and-rivers:2022.05.09-12.50.569dc0a4e492")
+    implementation("com.github.navikt:rapids-and-rivers:2024010209171704183456.6d035b91ffb4")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-server-websockets:$ktorVersion")
     implementation("io.ktor:ktor-server-call-id:$ktorVersion")
@@ -45,16 +52,16 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("net.logstash.logback:logstash-logback-encoder:7.1.1")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude("junit")
     }
     testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion") {
         exclude("junit")
     }
-    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("io.mockk:mockk:1.13.9")
 }
 
 tasks {

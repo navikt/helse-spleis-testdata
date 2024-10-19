7fec3f026c1e13f0fc82b3866f7b105e03ae4861
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-10-19 14:02:36 +0200

    ♻️ Splitter useEffect i useSubscribe i tre deler
    - Toast "Dokumenter er sendt" kommer nå bare når man submitter skjemaet,
      ikke når SSE-tilkoblingen blir gjenopprettet
    - Oppretting og lukking av EventSource per fnr er i en egen useEffect
    - Håndtering av SSE-meldinger i sin egen useEffect
    
    Og logging.

--- frontend/src/io/subscription.ts
+++ frontend/src/io/subscription.ts
@@ -19,49 +19,63 @@ type Message = {
 export const useSubscribe = (): UseSubscribeResult => {
   const [data, setData] = useState<{ fødselsnummer: string; key: string }>();
   const [tilstand, setTilstand] = useState<string>();
+  const [eventSource, setEventSource] = useState<EventSource>();
   const addMessage = useAddSystemMessage();
 
   useEffect(() => {
-    if (data?.fødselsnummer) {
-      const eventSource = new EventSource(`/sse/${data.fødselsnummer}`)
-
-      eventSource.addEventListener('open', () => {
-        addMessage({
-          id: nanoid(),
-          text: "Dokumenter er sendt. Venter på tilstandsendringer i spleis.",
-          timeToLiveMs: 5000,
-        });
-      })
-
-      eventSource.addEventListener('tilstandsendring', (event) => {
-        const message: Message = JSON.parse(event.data);
-        addMessage({
-          id: nanoid(),
-          text: `Vedtaksperioden har tilstand: ${message.tilstand}`,
-          dismissable: true,
-        });
-
-        switch (message.type) {
-          case MessageType.Endring: {
-            setTilstand(message.tilstand);
-            break;
-          }
-          default: {
-            console.error("Received unknown message:", message);
-          }
-        }
-      });
-
-      return () => {
-        eventSource.close();
-      };
-    }
+    if (!data?.fødselsnummer) return
+    addMessage({
+      id: nanoid(),
+      text: "Dokumenter er sendt.",
+      timeToLiveMs: 4000,
+    });
   }, [data]);
 
+  useEffect(() => {
+    if (!data?.fødselsnummer) return
+    const eventSource = new EventSource(`/sse/${data.fødselsnummer}`)
+    console.log(`eventSource opprettet: ${eventSource.url}`)
+    setEventSource(eventSource)
+
+    eventSource.addEventListener('open', () => {
+      console.log(`mottar meldinger for ${data.fødselsnummer}`);
+    })
+    eventSource.addEventListener('error', () => {
+      console.log(`feil på tilkobling for ${data.fødselsnummer}`);
+      eventSource.close()
+    })
+
+    return () => {
+      console.log("lukker eventSource " + eventSource.url);
+      eventSource.close();
+    };
+  }, [data?.fødselsnummer])
+
+  useEffect(() => {
+    if (!eventSource) return;
+    console.log('registrerer lyttere for tilstandsendring')
+    eventSource.addEventListener('tilstandsendring', (event) => {
+      const message: Message = JSON.parse(event.data);
+      addMessage({
+        id: nanoid(),
+        text: `Vedtaksperioden har tilstand: ${message.tilstand}`,
+        dismissable: true,
+      });
+
+      switch (message.type) {
+        case MessageType.Endring: {
+          setTilstand(message.tilstand);
+          break;
+        }
+        default: {
+          console.error("Received unknown message:", message);
+        }
+      }
+    });
+    }, [eventSource?.url]);
+
   return [
-    (fødselsnummer: string) => {
-      setData({ fødselsnummer, key: nanoid() });
-    },
+    (fødselsnummer: string) => setData({ fødselsnummer, key: nanoid() }),
     tilstand,
   ];
 };

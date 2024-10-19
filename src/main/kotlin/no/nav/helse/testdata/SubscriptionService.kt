413a7261de375059a5012f35557adbcc096f464b
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-10-19 11:01:42 +0200

    🔊 Litt innsiktslogging, for moro skyld

--- src/main/kotlin/no/nav/helse/testdata/SubscriptionService.kt
+++ src/main/kotlin/no/nav/helse/testdata/SubscriptionService.kt
@@ -11,10 +11,13 @@ internal interface SubscriptionService {
     fun update(fødselsnummer: String, nyTilstand: String)
 }
 
-internal object ConcreteSubscriptionService: SubscriptionService {
+internal object ConcreteSubscriptionService : SubscriptionService {
     private val subscriptions = mutableMapOf<String, MutableSharedFlow<EndringFrame>>()
+
     override fun addSubscription(fødselsnummer: String): SharedFlow<EndringFrame> {
         val flow = subscriptions.getOrPut(fødselsnummer) { MutableSharedFlow() }
+        log.info("Har subscriptions for ${subscriptions.size} fødselsnumre")
+        log.info("Subscription count for $fødselsnummer: ${flow.subscriptionCount.value}")
         return flow.asSharedFlow()
     }
 
